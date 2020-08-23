cfcaf343b04fd3e3fe26660303aa7383a4a5c0d2
Rene Cunningham <rene@compounddata.com>
2020-08-23 23:43:52 +1000

    Add config select one test.

--- internal/store/config_test.go
+++ internal/store/config_test.go
@@ -26,3 +26,32 @@ func TestConfigStoreCreate(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestConfigStoreSelectOne(t *testing.T) {
+	dbFile, db, err := testtooling.CreateTmpDB()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+	defer os.Remove(dbFile)
+
+	configStore := ConfigStore{db}
+
+	ctx := context.Background()
+	if _, err = configStore.SelectOne(ctx, 1, "doesNotExist"); err == nil {
+		t.Error("No error raised when calling configStore.SelectOne() with dummy option")
+	}
+
+	metricID, err := testtooling.CreateMetric(ctx, db)
+	if err != nil {
+		t.Error(err)
+	}
+
+	ret, err = configStore.SelectOne(ctx, 1, "data_type")
+	if err != nil {
+		t.Error(err)
+	}
+	if ret != "float" {
+		t.Errorf("want float but got %s", ret)
+	}
+}
