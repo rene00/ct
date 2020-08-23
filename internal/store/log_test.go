5b94e0c0c2fdaa321b3b4ba6a7c11a436ba6e5ff
Rene Cunningham <rene@compounddata.com>
2020-08-23 23:21:48 +1000

    More log test.

--- internal/store/log_test.go
+++ internal/store/log_test.go
@@ -2,34 +2,14 @@ package store
 
 import (
 	"context"
-	"ct/internal/storage"
-	"database/sql"
-	"fmt"
-	"io/ioutil"
+	"ct/internal/testtooling"
 	"os"
 	"testing"
 	"time"
 )
 
-func testSetup() (string, *sql.DB, error) {
-	tmpFile, err := ioutil.TempFile(os.TempDir(), "")
-	if err != nil {
-		return "", nil, err
-	}
-
-	dbFile := tmpFile.Name()
-	db, err := sql.Open("sqlite3", dbFile)
-	if err != nil {
-		return "", nil, err
-	}
-	if err := storage.DoMigrateDb(fmt.Sprintf("sqlite3://%s", dbFile)); err != nil {
-		return "", nil, err
-	}
-	return dbFile, db, nil
-}
-
-func TestLogStoreCreate(t *testing.T) {
-	dbFile, db, err := testSetup()
+func TestLogStoreUpsert(t *testing.T) {
+	dbFile, db, err := testtooling.CreateTmpDB()
 	if err != nil {
 		t.Error(err)
 	}
@@ -37,37 +17,8 @@ func TestLogStoreCreate(t *testing.T) {
 	defer os.Remove(dbFile)
 
 	ctx := context.Background()
-
-	tx, err := db.BeginTx(ctx, nil)
+	metricID, err := testtooling.CreateMetric(ctx, db)
 	if err != nil {
-		t.Error(err)
-	}
-
-	stmt, err := tx.PrepareContext(ctx, "INSERT INTO metric (id, name) VALUES (NULL, ?)")
-	if err != nil {
-		t.Error(err)
-	}
-
-	res, err := stmt.ExecContext(ctx, "test")
-	if err != nil {
-		t.Error(err)
-	}
-
-	metricID, err := res.LastInsertId()
-	if err != nil {
-		t.Error(err)
-	}
-
-	stmt, err = tx.PrepareContext(ctx, "INSERT INTO config (metric_id, opt, val) VALUES (?, ?, ?)")
-	if err != nil {
-		t.Error(err)
-	}
-
-	if _, err = stmt.ExecContext(ctx, metricID, "data_type", "float"); err != nil {
-		t.Error(err)
-	}
-
-	if err = tx.Commit(); err != nil {
 		t.Error(err)
 	}
 
@@ -78,12 +29,77 @@ func TestLogStoreCreate(t *testing.T) {
 
 	logStore := LogStore{db}
 
-	if err = logStore.Create(ctx, &Log{MetricID: metricID, Value: "1", Timestamp: ts}); err != nil {
+	if err = logStore.Upsert(ctx, &Log{MetricID: *metricID, Value: "1", Timestamp: ts}); err != nil {
 		t.Error(err)
 	}
 
-	if err = logStore.Create(ctx, &Log{MetricID: metricID, Value: "1", Timestamp: ts}); err == nil {
+	if err = logStore.Upsert(ctx, &Log{MetricID: *metricID, Value: "2", Timestamp: ts}); err != nil {
+		t.Error(err)
+	}
+
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	var ret int
+	err = tx.QueryRowContext(ctx, "SELECT COUNT(1) FROM log WHERE metric_id = ? AND value = 2 AND timestamp = ?", metricID, ts.Format("2006-01-02")).Scan(&ret)
+	if err != nil {
+		t.Errorf("Failed to select log: %v", err)
+	}
+	if ret != 1 {
+		t.Errorf("Incorrect number of log entries created: %v", ret)
+	}
+
+	if err = tx.Commit(); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestLogStoreCreate(t *testing.T) {
+	dbFile, db, err := testtooling.CreateTmpDB()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+	defer os.Remove(dbFile)
+
+	ctx := context.Background()
+	metricID, err := testtooling.CreateMetric(ctx, db)
+	if err != nil {
+		t.Error(err)
+	}
+
+	ts, err := time.Parse("2006-01-02", "2020-01-01")
+	if err != nil {
+		t.Error(err)
+	}
+
+	logStore := LogStore{db}
+
+	if err = logStore.Create(ctx, &Log{MetricID: *metricID, Value: "1", Timestamp: ts}); err != nil {
+		t.Error(err)
+	}
+
+	if err = logStore.Create(ctx, &Log{MetricID: *metricID, Value: "1", Timestamp: ts}); err == nil {
 		t.Error("Want err but err is nil when claling logstore.Create()")
 	}
 
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	var ret int
+	err = tx.QueryRowContext(ctx, "SELECT COUNT(1) FROM log WHERE metric_id = ? AND value = 1 AND timestamp = ?", metricID, ts.Format("2006-01-02")).Scan(&ret)
+	if err != nil {
+		t.Errorf("Failed to select log: %v", err)
+	}
+	if ret != 1 {
+		t.Errorf("Incorrect number of log entries created: %v", ret)
+	}
+
+	if err = tx.Commit(); err != nil {
+		t.Error(err)
+	}
 }
