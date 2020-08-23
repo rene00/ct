7a8dc3876adf65ebaa706ec9059ed0ae96399c00
Rene Cunningham <rene@compounddata.com>
2020-08-23 15:27:22 +1000

    Update migrations with new log table.

--- db/migrations/000002_log.down.sql
+++ db/migrations/000002_log.down.sql
@@ -1,1 +1,13 @@
+CREATE TABLE
+IF NOT EXISTS ct
+(
+    id INTEGER NOT NULL PRIMARY KEY,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    metric_id INTEGER NOT NULL,
+    value int NOT NULL,
+    FOREIGN KEY(metric_id) REFERENCES metric(id)
+);
+
+INSERT INTO ct(id, metric_id, value, timestamp) SELECT NULL, metric_id, value, timestamp FROM log;
+
 DROP TABLE IF EXISTS log;
--- db/migrations/000002_log.up.sql
+++ db/migrations/000002_log.up.sql
@@ -8,3 +8,7 @@ IF NOT EXISTS log
     FOREIGN KEY(metric_id) REFERENCES metric(id),
     UNIQUE(metric_id,timestamp)
 );
+
+INSERT INTO log(id, metric_id, value, timestamp) SELECT NULL, metric_id, value, timestamp FROM ct;
+
+DROP TABLE ct;
