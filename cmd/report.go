4e9b31ca01a34378e08eda3141aef8bd45a2c433
Rene Cunningham <rene@compounddata.com>
2020-11-24 21:35:11 +1100

    Start to add weekly report support

--- cmd/report.go
+++ cmd/report.go
@@ -156,6 +156,64 @@ var reportMonthlyCmd = &cobra.Command{
 	},
 }
 
+var reportWeeklyCmd = &cobra.Command{
+	Use:   "weekly [command]",
+	Short: "run the weekly report",
+	PreRun: func(cmd *cobra.Command, args []string) {
+		_ = viper.BindPFlag("metric", cmd.Flags().Lookup("metric"))
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg, err := config.NewConfig(cmd.Flags())
+		if err != nil {
+			fmt.Fprintf(os.Stderr, fmt.Sprintf("%v\n", err))
+			os.Exit(1)
+		}
+
+		db, err := sql.Open("sqlite3", cfg.UserViperConfig.GetString("ct.db_file"))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, fmt.Sprintf("%v\n", err))
+			os.Exit(1)
+		}
+		defer db.Close()
+
+		s := store.NewStore(db)
+		ctx := context.Background()
+		metric, err := s.Metric.SelectOne(ctx, viper.GetString("metric"))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, fmt.Sprintf("%v\n", err))
+			os.Exit(1)
+		}
+
+		configMetricType, err := s.Config.SelectOne(ctx, metric.MetricID, "metric_type")
+		if err != nil && err != store.ErrNotFound {
+			fmt.Fprintf(os.Stderr, fmt.Sprintf("%v\n", err))
+			os.Exit(1)
+		}
+		if err != nil && err == store.ErrNotFound {
+			fmt.Fprintf(os.Stderr, fmt.Sprintf("Missing config option metric_type: %s", metric.Name))
+			os.Exit(1)
+		}
+
+		switch configMetricType {
+		case "counter":
+			if err = report.WeeklyCounter(ctx, db, metric); err != nil {
+				fmt.Fprintf(os.Stderr, fmt.Sprintf("%v\n", err))
+				os.Exit(1)
+			}
+		case "gauge":
+			if err = report.WeeklyGauge(ctx, db, metric); err != nil {
+				fmt.Fprintf(os.Stderr, fmt.Sprintf("%v\n", err))
+				os.Exit(1)
+			}
+		default:
+			fmt.Fprintf(os.Stderr, fmt.Sprintf("Unsupported reporting for metric type: %s", configMetricType))
+			os.Exit(1)
+		}
+
+		return nil
+	},
+}
+
 func initReportDailyCmd() {
 	c := reportDailyCmd
 	f := c.Flags()
@@ -172,6 +230,14 @@ func initReportMonthlyCmd() {
 	f.String("config-file", "", "")
 }
 
+func initReportWeeklyCmd() {
+	c := reportWeeklyCmd
+	f := c.Flags()
+	f.String("metric", "", "Metric")
+	c.MarkFlagRequired("metric")
+	f.String("config-file", "", "")
+}
+
 func initReportStreakCmd() {
 	c := reportStreakCmd
 	f := c.Flags()
@@ -182,9 +248,11 @@ func initReportStreakCmd() {
 
 func init() {
 	initReportDailyCmd()
+	initReportWeeklyCmd()
 	initReportMonthlyCmd()
 	initReportStreakCmd()
 	reportCmd.AddCommand(reportDailyCmd)
+	reportCmd.AddCommand(reportWeeklyCmd)
 	reportCmd.AddCommand(reportMonthlyCmd)
 	reportCmd.AddCommand(reportStreakCmd)
 	rootCmd.AddCommand(reportCmd)
